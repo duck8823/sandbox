d90723001f7e87fa61cb83f8d72be6e6aed165f0
shunsuke maeda <duck8823@gmail.com>
2018-09-04 11:18:03 +0900

    Update script to output to file

--- source/scripts/dummy_ltsv.pl
+++ source/scripts/dummy_ltsv.pl
@@ -4,11 +4,16 @@
 use strict;
 use warnings;
 
+use IO::Handle;
+
+open(TXT, '>/var/log/dummy_ltsv.txt') or die "$!";
+TXT->autoflush(1);
 for (my $i = 0;;$i++) {
     my $number;
     for (1..10) {
         $number .= int(rand(10));
     }
-    print "index:$i\tnumber:$number\n";
+    print TXT "index:$i\tnumber:$number\n";
     sleep 1;
 }
+close(TXT);
